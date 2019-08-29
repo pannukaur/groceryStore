1c56753cfcc89a55fb0cebdf3ab0f584602b3423
pannukaur <ssingh9442@yahoo.com>
2019-08-28 21:00:39 -0700

    grocery store day one is done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,47 @@
-const storeName = '';
+const storeName = 'Gucci';
 document.title = `${storeName} Grocery Store`;
+console.log('What is a document:', document.all[24]);
+console.log('tag element:',document.getElementsByTagName('p'));
+let h1_OfHeader = document.getElementById('mainIntro');
+// console.log(h1_OfHeader);
+
+h1_OfHeader.innerHTML ='Welcome';
+
+const getAllNavListClass = document.querySelectorAll('.nav_list_item');
+console.log('headers:',
+getAllNavListClass);
+
+const getIconCredit = document.querySelector('#iconsCredit');
+const testDifference = document.getElementById('iconsCredit');
+console.log('get the id:', getIconCredit);
+console.log(testDifference);
+
+const getClasses = document.getElementsByClassName('nav_list_item');
+console.log(getClasses);
+
+const imageExample = document.querySelector('img[alt="cherry icon"]');
+console.log(imageExample);
+
+const magicButton = document.getElementById('magic');
+console.log(magicButton);
+
+magicButton.addEventListener('mouseover', () => {
+magicButton.className = 'myPoint';
+magicButton.style.backgroundColor = "#000";
+console.log(magicButton);
+});
+
+magicButton.addEventListener('mouseleave', () => {
+    magicButton.className ="";
+    console.log('leaving:', magicButton);
+});
+
+document.addEventListener('keyup', ()=> {
+    console.log(event.keycode);
+});
+const button = document.getElementById('content');
+
+button.addEventListener('mouseover', () =>{
+    button.style.backgroundColor = "blue";
+    console.log('hi');
+})
